81185fd4a67a33dd27ad5491cedb2170bda6070e
tunogya <tunogya@qq.com>
2022-08-01 14:31:26 +0800

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Stack, Text} from "@chakra-ui/react";
+
+export default function Home(): JSX.Element {
+  return (
+    <Stack>
+      <Text>Text</Text>
+    </Stack>
+  );
+}
